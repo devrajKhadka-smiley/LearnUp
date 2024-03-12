3ba1a6ed776d648a8cac162ace8192ab8f278fd4
devrajkhadka-smiley <devrajkhadka941@gmail.com>
2024-03-12 07:47:47 +0545

    only footer left

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const header = document.querySelector("header");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
